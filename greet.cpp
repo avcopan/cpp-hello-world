aff425b06bc23241de0be7bc24ec86e86b908156
avcopan <avcopan@gmail.com>
2018-06-07 10:43:46 -0500

    Adds static code checks with cppcheck.  This build should fail.

--- greet.cpp
+++ greet.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 
 int main() {
+    /* Comment these lines in to fail the cppcheck: */
+    int a[3] = {0, 1, 2};
+    a[3] = 4;
     std::cout << "Hello world!" << std::endl;
 }
