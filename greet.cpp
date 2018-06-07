d18efaeb93a5fd76a242ae27345cfcf63f20b669
avcopan <avcopan@gmail.com>
2018-06-07 10:51:59 -0500

    Now the build should pass.

--- greet.cpp
+++ greet.cpp
@@ -2,7 +2,7 @@
 
 int main() {
     /* Comment these lines in to fail the cppcheck: */
-    int a[3] = {0, 1, 2};
-    a[3] = 4;
+    // int a[3] = {0, 1, 2};
+    // a[3] = 4;
     std::cout << "Hello world!" << std::endl;
 }
